7e61630a97c6c526096d9b67858fc559257b543c
$ <ram.marvaro@gmail.com>
2020-01-11 14:33:30 -0800

    allowed hosts production update

--- the_blog/settings.py
+++ the_blog/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = '3$xfaxzl0gh3l4_tv*r5ujami%m-l0@yo)m6hp2eq3c55c(k-!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'zonesoccer.herokuapp.com',
-    '127.0.0.1'
-
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
